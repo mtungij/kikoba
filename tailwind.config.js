644f1b30339654a271e83118fd70c98c5c4f1e6e
mtungij <jamesmtungi2013@gmail.com>
2024-09-03 15:20:43 +0300

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 
 const defaultTheme = require("tailwindcss/defaultTheme");
 const forms = require("@tailwindcss/forms");
-import preset from './vendor/filament/support/tailwind.config.preset'
+
 module.exports = {
     darkMode: "class", // or 'media', depending on your preference
-    presets: [
-        require('./vendor/wireui/wireui/tailwind.config.js')
-    ],
+   
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
         "./node_modules/flowbite/**/*.js",
         './vendor/masmerise/livewire-toaster/resources/views/*.blade.php', // 👈
-        './vendor/wireui/wireui/resources/**/*.blade.php',
-        './vendor/wireui/wireui/ts/**/*.ts',
-        './vendor/wireui/wireui/src/View/**/*.php',
+       
+       
           
     ],
     theme: {
