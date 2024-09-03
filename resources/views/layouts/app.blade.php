c52948e717eb3e4e19b0d23c75fca1c6857c247b
mtungij <jamesmtungi2013@gmail.com>
2024-09-03 16:06:44 +0300

    url

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
+<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
         <title>{{ config('app.name', 'Laravel') }}</title>
 
     <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
