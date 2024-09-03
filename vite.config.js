140877fc10317505d0c3d79d539ac2dae59eb737
mtungij <jamesmtungi2013@gmail.com>
2024-09-03 15:50:12 +0300

    ..

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        https: true, // Set this to true for development HTTPS
+    },
     plugins: [
         laravel({
             input: [
