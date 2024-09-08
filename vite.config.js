563223bf7ffe5fd73d70d05998f128d0d3cb45bd
mtungij <jamesmtungi2013@gmail.com>
2024-09-08 18:24:18 +0300

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     server: {
-        https: true, // Use this for development HTTPS
+        https: true, // Use this for development HTTPS; set to false for non-HTTPS
+        proxy: {
+            // Add a proxy if you need to proxy API requests during development
+            '/api': 'http://localhost',
+        },
     },
     build: {
-        // Adjust if needed
         assetsDir: 'build/assets',
         assetsInlineLimit: 0,
     },
