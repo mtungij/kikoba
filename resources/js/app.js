40594f96bab935327cdbdd9ed5ca7b39cc5173b9
mtungij <jamesmtungi2013@gmail.com>
2024-09-08 18:51:09 +0300

    flowbite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,19 +1,20 @@
 import './bootstrap';
 import 'flowbite';
-
 import 'select2';
 import 'select2/dist/css/select2.min.css';
-
-import '../../vendor/masmerise/livewire-toaster/resources/js'; // 
+import '../../vendor/masmerise/livewire-toaster/resources/js';
 import { initFlowbite } from 'flowbite';
 
-
-
 document.addEventListener('DOMContentLoaded', () => {
-   console.log("dom loaded");
+   console.log("DOM fully loaded and parsed");
 });
 
-document.addEventListener('livewire:navigated', () => { 
-    console.log("Navigated");
-    initFlowbite();
-})
+document.addEventListener('livewire:load', () => {
+    console.log("Livewire component loaded");
+    initFlowbite(); // Initialize Flowbite on Livewire load
+});
+
+document.addEventListener('livewire:updated', () => { 
+    console.log("Livewire component updated");
+    initFlowbite(); // Reinitialize Flowbite after Livewire updates the DOM
+});
