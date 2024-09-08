e5ab2e70caa737a89ff9f033d92e74a7585c77a2
mtungij <jamesmtungi2013@gmail.com>
2024-09-09 00:55:58 +0300

    sms

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,32 +9,14 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 Alpine.start();
 
-// Import Flowbite and ensure it is initialized if necessary
-import { initFlowbite } from 'flowbite';
-
-// Expose initFlowbite globally
-window.initFlowbite = initFlowbite;
-
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("DOM fully loaded and parsed");
-    // Optionally initialize Flowbite here if needed
-    if (typeof window.initFlowbite === 'function') {
-        window.initFlowbite();
-    }
+document.addEventListener("livewire:navigating", () => {
+    // Mutate the HTML before the page is navigated away...
+    initFlowbite();
 });
 
-document.addEventListener('livewire:load', () => {
-    console.log("Livewire component loaded");
-    // Ensure Flowbite initialization on Livewire component load
-    if (typeof window.initFlowbite === 'function') {
-        window.initFlowbite();
-    }
+document.addEventListener("livewire:navigated", () => {
+    // Reinitialize Flowbite components
+    initFlowbite();
 });
 
-document.addEventListener('livewire:updated', () => { 
-    console.log("Livewire component updated");
-    // Ensure Flowbite reinitialization after Livewire updates
-    if (typeof window.initFlowbite === 'function') {
-        window.initFlowbite();
-    }
-});
+
